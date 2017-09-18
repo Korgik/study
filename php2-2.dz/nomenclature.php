c3fc12b9fe7a61e116f5dad054a835463a71baeb
Korgik <akinfeev-3@mail.ru>
2017-09-18 12:03:10 +0300

    Добавлен интерфейс

--- php2-2.dz/nomenclature.php
+++ php2-2.dz/nomenclature.php
@@ -16,6 +16,16 @@ public class DigitalItems extends AbstractItem implements Income{
 	{
 		
 	}
+	public function RetailPrice(){
+		// Цена розничная
+	}
+	public function PurchasePrice(){
+		// Цена закупочная
+	}
+	public function Difference(){
+		// Доход с продаж с учетом особенностей товара
+	}
+
 }
 // Штучный физический товар. Обычная стоймость
 public class Item extends AbstractItem implements Income{
@@ -23,11 +33,31 @@ public class Item extends AbstractItem implements Income{
 	{
 		# code...
 	}
+		}
+	public function RetailPrice(){
+		// Цена розничная
+	}
+	public function PurchasePrice(){
+		// Цена закупочная
+	}
+	public function Difference(){
+		// Доход с продаж с учетом особенностей товара
+	}
 }
 // Товар на вес. В зависимости от продаваемого количества в килограммах
 public class ItemWeight extends AbstractItem implements Income{
 	public function IWIncome($IWVelue=400)
 	{
 		# code...
+	}
+		}
+	public function RetailPrice(){
+		// Цена розничная
+	}
+	public function PurchasePrice(){
+		// Цена закупочная
+	}
+	public function Difference(){
+		// Доход с продаж с учетом особенностей товара
 	}
 }
