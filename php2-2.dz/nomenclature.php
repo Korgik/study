43fc07305bf17e4c60414b726fae5f36503083c3
Korgik <akinfeev-3@mail.ru>
2017-09-18 11:57:49 +0300

    Структура номенклатуры

---
+++ php2-2.dz/nomenclature.php
@@ -0,0 +1,33 @@
+<?
+// Интерфейс для подсчета дохода с продажи. Каждый метод будет работать в своем классе.
+interface Income{
+	public function RetailPrice();
+	public function PurchasePrice();
+	public function Difference();
+}
+
+// Абстрактный класс товара и его метод(!)
+public abstract class AbstractItem implements Income{
+	public absrtact function AbstractIncome($AbstractValue=100);	
+}
+// Цифровой товар. стоймость постоянная(статическая) и дешевле штучного в два раза.
+public class DigitalItems extends AbstractItem implements Income{
+	public static function DigitalIncome($DigitalValue=150)
+	{
+		
+	}
+}
+// Штучный физический товар. Обычная стоймость
+public class Item extends AbstractItem implements Income{
+	public function ItemIncome($ItemValue=300)
+	{
+		# code...
+	}
+}
+// Товар на вес. В зависимости от продаваемого количества в килограммах
+public class ItemWeight extends AbstractItem implements Income{
+	public function IWIncome($IWVelue=400)
+	{
+		# code...
+	}
+}
